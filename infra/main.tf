20699b1b52ee5e932ee115c5b599677bf349913c
paprika-mah <paprika.mah@gmail.com>
2022-07-17 13:07:47 +0900

    minor fix

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,8 @@
 resource "aws_cognito_user_pool" "pool" {
-  name = "mypool"
+  name = "congnito-sample-user-pool"
 }
 
 resource "aws_cognito_user_pool_client" "client" {
-  name = "client"
-
-  user_pool_id = aws_cognito_user_pool.pool.id
+  name          = "cognito-sample-user-pool-app-client"
+  user_pool_id  = aws_cognito_user_pool.pool.id
 }
